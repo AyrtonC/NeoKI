3264f1979513d96789be960cbffff6eff31c0d03
Ayrton Cavalieri de Almeida <ayrtoncavalieri@gmail.com>
2016-09-16 01:04:03 -0300

    Removed some comments.

--- NeoKI/BBuffer.cpp
+++ NeoKI/BBuffer.cpp
@@ -1,11 +1,11 @@
 // Copyright [2016] [Ayrton Cavalieri de Almeida]
- 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
- 
+//
 // http://www.apache.org/licenses/LICENSE-2.0
- 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,14 +16,13 @@
 
 BBuffer::BBuffer(std::size_t sizeOfBuffer, std::size_t sizeOfType) : _empty(sizeOfBuffer), _full(0), _toSend(0), _sent(0), _sizeOfBuffer(sizeOfBuffer), _sizeOfType(sizeOfType), info(new uint8_t[sizeOfBuffer * sizeOfType])
 {
-    //info = calloc(sizeOfBuffer, sizeOfType);
+    
 }
 
 BBuffer::~BBuffer()
 {
     uint8_t *ptr = (uint8_t*)info;
     delete [] ptr;
-    //free(info);
 }
 
 void BBuffer::enqueue(void *buf)
