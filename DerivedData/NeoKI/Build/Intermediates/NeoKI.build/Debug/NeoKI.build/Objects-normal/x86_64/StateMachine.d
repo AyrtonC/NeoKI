4151376f1754728a08247a17dfec4db75a65a8e3
Ayrton Cavalieri de Almeida <ayrtoncavalieri@gmail.com>
2016-09-19 01:28:22 -0300

    Log updates.

---
+++ DerivedData/NeoKI/Build/Intermediates/NeoKI.build/Debug/NeoKI.build/Objects-normal/x86_64/StateMachine.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/ayrtex/Documents/Habilite/NeoKI/NeoKI/StateMachine.cpp \
+  /Users/ayrtex/Documents/Habilite/NeoKI/NeoKI/StateMachine.hpp \
+  /Users/ayrtex/Documents/Habilite/NeoKI/NeoKI/SerialCom.hpp
